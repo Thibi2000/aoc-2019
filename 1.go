4828f332aff08a48237fd20600e3906e14700b95
Thibault Cools <thibaultcools@outlook.be>
2019-12-01 12:16:34 +0100

    first challenge

---
+++ 1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "strconv"
+)
+
+func do_each_line(filename string, start_val int,  action func(line string)int ) int {
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Println("Can't open file")
+    }
+    defer file.Close()
+    
+    scanner := bufio.NewScanner(file)
+    res := start_val
+    for scanner.Scan() {
+        res += action(scanner.Text())
+    }
+    
+    if err := scanner.Err(); err != nil {
+        fmt.Println("Couldn't read file")
+    }
+
+    return res
+}
+
+func main() {
+    part1 := do_each_line("inputs/1.txt", 0, func(line string) int {
+        mass, err := strconv.Atoi(line)
+        if err != nil {
+            fmt.Println("Couldn't convert text to int")
+        }
+        return mass / 3 - 2
+    })
+    part2 := do_each_line("inputs/1.txt", 0, func(line string) int {
+        mass, err := strconv.Atoi(line)
+        if err != nil {
+            fmt.Println("Couldn't convert text to int")
+        }
+        res := 0
+        for f:= mass/3 - 2; f > 0; f = f / 3 - 2 {
+            res += f
+        }
+        return res
+    })
+    fmt.Printf("Part1: %d\n", part1)
+    fmt.Printf("Part2: %d\n", part2)
+}
